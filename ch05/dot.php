8a3c4f9f160b63e7a6b92cad3d5078090db271a6
ToBeNew <crebiz76@gmail.com>
2020-12-17 01:16:44 +0900

    complete ch05

---
+++ ch05/dot.php
@@ -0,0 +1,11 @@
+<?php
+    // 휴대폰 번호: 010-1234-5678
+    $phone1 = "010";
+    $phone2 = "1234";
+    $phone3 = "5678";
+
+    // print "휴대폰 번호: ".$phone1."-".$phone2."-".$phone3;
+    // or
+    $phone = $phone1."-".$phone2."-".$phone3;
+    print "휴대폰 번호: ".$phone;
+?>
